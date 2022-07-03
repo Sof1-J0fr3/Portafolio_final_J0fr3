a1909fd307f1e45d92ed6adc6ba28ef8c4f205ef
Sof1-J0fr3 <104612024+Sof1-J0fr3@users.noreply.github.com>
2022-07-03 17:59:43 -0300

    creación de headder y banner

--- Proyect/src/app/app.module.ts
+++ Proyect/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { RedesComponent } from './componentes/redes/redes.component';
+import { BannerComponent } from './componentes/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    RedesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
